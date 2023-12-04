0878a717f64601855e251e26131a3921f20ba141
Paul Chambaz <paul.chambaz@tutanota.com>
2023-12-04 01:44:15 +0100

    wrong for task 1 but shows speed improvement

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -19,34 +19,46 @@ fn main() {
     println!("Task 2: {}", task_2);
 }
 
+#[derive(Clone, Copy)]
 struct Engine {
     number: i32,
     start: i32,
     end: i32,
-    y: i32,
 }
 
+#[derive(Clone, Copy, Debug)]
 struct Power {
     x: i32,
-    y: i32,
 }
 
-fn create_engine(str: String, start: i32, end: usize, y: usize) -> Engine {
-    Engine {
-        number: str.parse::<i32>().expect("Error trying to parse number"),
-        start,
-        end: (end - 1) as i32,
-        y: y as i32,
+fn aabb(a_start: i32, a_end: i32, b_x: i32) -> bool {
+    a_start - 1 <= b_x && a_end + 1 >= b_x
+}
+
+fn merge_items<T: Clone>(items: &[Vec<T>], y: usize) -> Vec<T> {
+    let mut merged = Vec::new();
+    if y as i32 > 0 {
+        if let Some(prev) = items.get(y - 1) {
+            merged.extend_from_slice(prev);
+        }
     }
-}
-
-fn aabb(a_start: i32, a_end: i32, a_y: i32, b_x: i32, b_y: i32) -> bool {
-    a_y - 1 <= b_y && a_y + 1 >= b_y && a_start - 1 <= b_x && a_end + 1 >= b_x
+    if let Some(current) = items.get(y) {
+        merged.extend_from_slice(current);
+    }
+    if y + 1 < items.len() {
+        if let Some(next) = items.get(y + 1) {
+            merged.extend_from_slice(next);
+        }
+    }
+    merged
 }
 
 fn task_1(file: String) -> u32 {
-    let mut engines: Vec<Engine> = Vec::new();
-    let mut powers: Vec<Power> = Vec::new();
+    let n = file.lines().count();
+    let mut engines: Vec<Vec<Engine>> = Vec::with_capacity(n);
+    for _ in 0..n { engines.push(Vec::new()); }
+    let mut powers: Vec<Vec<Power>> = Vec::with_capacity(n);
+    for _ in 0..n { powers.push(Vec::new()); }
     for (y, line) in file.lines().enumerate() {
         let mut str = String::new();
         let mut start = -1;
@@ -59,32 +71,50 @@ fn task_1(file: String) -> u32 {
             } else if "#%&*+-/=@$".contains(char) {
                 let power = Power {
                     x: x as i32,
-                    y: y as i32,
                 };
-                powers.push(power);
+                powers[y].push(power);
                 if start != -1 {
-                    engines.push(create_engine(str, start, x, y));
-                    str = String::new();
+                    let engine = Engine {
+                        number: str.parse::<i32>().expect("Error trying to parse number"),
+                        start,
+                        end: (x - 1) as i32,
+                    };
+                    engines[y].push(engine);
+                    str.clear();
                     start = -1;
                 }
             } else if start != -1 {
-                engines.push(create_engine(str, start, x, y));
-                str = String::new();
+                let engine = Engine {
+                    number: str.parse::<i32>().expect("Error trying to parse number"),
+                    start,
+                    end: (x - 1) as i32,
+                };
+                engines[y].push(engine);
+                str.clear();
                 start = -1;
             }
         }
 
         if start != -1 {
-            engines.push(create_engine(str, start, line.len(), y));
+            let engine = Engine {
+                number: str.parse::<i32>().expect("Error trying to parse number"),
+                start,
+                end: (str.len() - 1) as i32,
+            };
+            engines[y].push(engine);
         }
     }
 
     let mut sum = 0;
-    for engine in &engines {
-        for power in &powers {
-            if aabb(engine.start, engine.end, engine.y, power.x, power.y) {
-                sum += engine.number;
-                break;
+    for (y, engine_group) in engines.into_iter().enumerate() {
+        let merged = merge_items::<Power>(&powers, y);
+        println!("{} {:?}", y, merged);
+        for engine in engine_group {
+            for power in &merged {
+                if aabb(engine.start, engine.end, power.x) {
+                    sum += engine.number;
+                    break;
+                }
             }
         }
     }
@@ -93,8 +123,11 @@ fn task_1(file: String) -> u32 {
 }
 
 fn task_2(file: String) -> u32 {
-    let mut engines: Vec<Engine> = Vec::new();
-    let mut powers: Vec<Power> = Vec::new();
+    let n = file.lines().count();
+    let mut engines: Vec<Vec<Engine>> = Vec::with_capacity(n);
+    for _ in 0..n { engines.push(Vec::new()); }
+    let mut powers: Vec<Vec<Power>> = Vec::with_capacity(n);
+    for _ in 0..n { powers.push(Vec::new()); }
     for (y, line) in file.lines().enumerate() {
         let mut str = String::new();
         let mut start = -1;
@@ -107,39 +140,54 @@ fn task_2(file: String) -> u32 {
             } else if char == '*' {
                 let power = Power {
                     x: x as i32,
-                    y: y as i32,
                 };
-                powers.push(power);
+                powers[y].push(power);
                 if start != -1 {
-                    engines.push(create_engine(str, start, x, y));
-                    str = String::new();
+                    let engine = Engine {
+                        number: str.parse::<i32>().expect("Error trying to parse number"),
+                        start,
+                        end: (x - 1) as i32,
+                    };
+                    engines[y].push(engine);
+                    str.clear();
                     start = -1;
                 }
             } else if start != -1 {
-                engines.push(create_engine(str, start, x, y));
-                str = String::new();
+                let engine = Engine {
+                    number: str.parse::<i32>().expect("Error trying to parse number"),
+                    start,
+                    end: (x - 1) as i32,
+                };
+                engines[y].push(engine);
+                str.clear();
                 start = -1;
             }
         }
 
         if start != -1 {
-            engines.push(create_engine(str, start, line.len(), y));
+            let engine = Engine {
+                number: str.parse::<i32>().expect("Error trying to parse number"),
+                start,
+                end: (str.len() - 1) as i32,
+            };
+            engines[y].push(engine);
         }
     }
 
-    // validate the engines
     let mut sum = 0;
-    for power in &powers {
-        let mut ratios: Vec<i32> = Vec::new();
-        for engine in &engines {
-            if aabb(engine.start, engine.end, engine.y, power.x, power.y) {
-                ratios.push(engine.number);
+    for (y, power_group) in powers.into_iter().enumerate() {
+        let merged = merge_items::<Engine>(&engines, y);
+        for power in power_group {
+            let mut ratios: Vec<i32> = Vec::new();
+            for engine in &merged {
+                if aabb(engine.start, engine.end, power.x) {
+                    ratios.push(engine.number);
+                }
             }
-        }
-
-        if ratios.len() == 2 {
-            let product: i32 = ratios.iter().product();
-            sum += product;
+            if ratios.len() == 2 {
+                let product: i32 = ratios.iter().product();
+                sum += product;
+            }
         }
     }
 
